b1115e6fea2a053d8e8222fb41504b065997b2c1
erick <erickportugal874@gmail.com>
2020-11-13 14:54:01 -0300

    exercicios

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -3,6 +3,10 @@ package classe;
 public class Data {
 
 	int dia;
-	String mes;
+	int mes;
 	int ano;
+	
+	String obeterDataFormatada() {
+		return String.format("%d/%d/%d", dia, mes, ano);
+	}
 }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -6,15 +6,15 @@ public class DataTeste {
 		
 		Data d1 = new Data();
 		d1.dia = 9;
-		d1.mes = "Novembro";
+		d1.mes = 11;
 		d1.ano = 2020;
 		
 		var d2 = new Data();
 		d2.dia = 10;
-		d2.mes = "Dezembro";
+		d2.mes = 12;
 		d2.ano = 2021;
 		
-		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
-		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		System.out.println(d1.obeterDataFormatada());
+		System.out.println(d2.obeterDataFormatada());
 	}
 }
